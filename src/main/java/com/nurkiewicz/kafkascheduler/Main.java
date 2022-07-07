d816a3adba49ec83f4deedfeb2f95a56d0790464
Tomasz Nurkiewicz <tnr@devskiller.com>
2022-07-07 15:04:21 +0200

    Receiving and sending back to the same time bucket

--- src/main/java/com/nurkiewicz/kafkascheduler/Main.java
+++ src/main/java/com/nurkiewicz/kafkascheduler/Main.java
@@ -1,20 +1,20 @@
 package com.nurkiewicz.kafkascheduler;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
 
 public class Main {
 
@@ -24,27 +24,53 @@ public class Main {
 		String bootstrapServers = "127.0.0.1:9092";
 		String groupId = "kafka-scheduler";
 		String topic = "quickstart";
-		Properties properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		//creating consumer
-		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
+		Properties consumerProps = new Properties();
+		consumerProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		consumerProps.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		consumerProps.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		consumerProps.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		consumerProps.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		int partition = 12;
+		try (KafkaProducer<String, String> producer = buildProducer(bootstrapServers);
+		     KafkaConsumer<String, String> consumer = buildConsumer(bootstrapServers, groupId)) {
 			int count = consumer.partitionsFor(topic).size();
 			log.info("{} partitions for {}", count, topic);
-			TopicPartition timeBucket = new TopicPartition(topic, 12);
+			TopicPartition timeBucket = new TopicPartition(topic, partition);
 			consumer.assign(List.of(timeBucket));
 			//polling
 			while (true) {
 				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 				for (ConsumerRecord<String, String> record : records) {
-					log.info("Key: " + record.key() + ", Value:" + record.value());
-					log.info("Partition:" + record.partition() + ",Offset:" + record.offset());
+					log.info("Processing {}", record);
+					producer.send(new ProducerRecord<>(topic, partition, record.key(), record.value()));
 				}
 				consumer.commitSync();
 			}
 		}
+	}
+
+	private static KafkaConsumer<String, String> buildConsumer(String bootstrapServers, String groupId) {
+		Properties consumerProps = new Properties();
+		consumerProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		consumerProps.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		consumerProps.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		consumerProps.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		consumerProps.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		return new KafkaConsumer<>(consumerProps);
+	}
+
+	private static KafkaProducer<String, String> buildProducer(String bootstrapServers) {
+		Properties producerProps = new Properties();
+		producerProps.put("retries", 3);
+		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		producerProps.put(ProducerConfig.ACKS_CONFIG, "1");
+		producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		producerProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 120000);
+		producerProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 1024);
+		producerProps.put(ProducerConfig.LINGER_MS_CONFIG, 0);
+		producerProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 1024);
+		producerProps.put("compression.type", "gzip");
+		return new KafkaProducer<>(producerProps);
 	}
 }
